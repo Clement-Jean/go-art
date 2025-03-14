90f44271172848df87a1c4671087d2283d5730d9
Clement <clement.jean@epitech.eu>
2025-03-14 22:30:02 +0800

    add fallback node16Search

--- node16.go
+++ node16.go
@@ -1,3 +1,5 @@
+//go:build amd64 || arm64
+
 package art
 
 func searchNode16(keys *[16]byte, childrenLen uint8, b byte) int
---
+++ node16_other.go
@@ -0,0 +1,22 @@
+//go:build !amd64 && !arm64
+
+package art
+
+import "math/bits"
+
+func searchNode16(keys *[16]byte, childrenLen uint8, b byte) int {
+	bitfield := 0
+	for i := range 16 {
+		if keys[i] == b {
+			bitfield |= (1 << i)
+		}
+	}
+
+	mask := (1 << childrenLen) - 1
+	bitfield &= mask
+
+	if bitfield != 0 {
+		return bits.TrailingZeros(uint(bitfield))
+	}
+	return -1
+}
