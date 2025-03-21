8cb4b494f0630095dd17e34fb40e5adb7d70c8a4
Clement <clement.jean@epitech.eu>
2025-03-21 17:46:17 +0800

    add links to other ART libraries

--- tree_test.go
+++ tree_test.go
@@ -43,13 +43,13 @@ func loadTestFile(path string) [][]byte {
 // Tree Insert UUIDs       64    16,935,374 ns/op   14,479,164 B/op    300,664 allocs/op
 // Tree Search UUIDs       69    14,798,726 ns/op    9,600,019 B/op    200,000 allocs/op
 
-// go-adaptive-radix-tree
+// go-adaptive-radix-tree (https://github.com/plar/go-adaptive-radix-tree/tree/master)
 // Tree Insert Words	    9	117,888,698 ns/op	37,942,744 B/op	 1,214,541 allocs/op
 // Tree Search Words	   26	 44,555,608 ns/op	         0 B/op	         0 allocs/op
 // Tree Insert UUIDs	   18	 59,360,135 ns/op	18,375,723 B/op	   485,057 allocs/op
 // Tree Search UUIDs	   54	 21,265,931 ns/op	         0 B/op	         0 allocs/op
 
-// go-art
+// go-art (https://github.com/kellydunn/go-art)
 // Tree Insert Words	    5	272,047,975 ns/op	81,628,987 B/op	 2,547,316 allocs/op
 // Tree Search Words	   10	129,011,177 ns/op	13,272,278 B/op	 1,659,033 allocs/op
 // Tree Insert UUIDs	   10	140,309,246 ns/op	33,678,160 B/op	   874,561 allocs/op
