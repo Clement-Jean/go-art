52267cad19f9ff33bdda0f433ba0fa5a6c1437f8
Clement <clement.jean@epitech.eu>
2025-03-31 17:44:16 +0800

    generate

--- nodekind_string.go
+++ nodekind_string.go
@@ -8,17 +8,16 @@ func _() {
 	// An "invalid array index" compiler error signifies that the constant values have changed.
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
-	_ = x[nodeKindUndefined-0]
-	_ = x[nodeKindLeaf-1]
-	_ = x[nodeKind4-2]
-	_ = x[nodeKind16-3]
-	_ = x[nodeKind48-4]
-	_ = x[nodeKind256-5]
+	_ = x[nodeKind4-0]
+	_ = x[nodeKind16-1]
+	_ = x[nodeKind48-2]
+	_ = x[nodeKind256-3]
+	_ = x[nodeKindLeaf-4]
 }
 
-const _nodeKind_name = "UNDEFINEDNODE_LEAFNODE_4NODE_16NODE_48NODE_256"
+const _nodeKind_name = "NODE_4NODE_16NODE_48NODE_256NODE_LEAF"
 
-var _nodeKind_index = [...]uint8{0, 9, 18, 24, 31, 38, 46}
+var _nodeKind_index = [...]uint8{0, 6, 13, 20, 28, 37}
 
 func (i nodeKind) String() string {
 	if i >= nodeKind(len(_nodeKind_index)-1) {
